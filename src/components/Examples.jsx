3134f196fcb51a512a0dbdc19e7372da77f891db
Alex Brilevski <brilevski.alex@gmail.com>
2025-08-24 11:59:07 +0300

    Components have been split by feature and state

---
+++ src/components/Examples.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import TabButton from './TabButton';
+import { EXAMPLES } from '../data';
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  const handleTabButtonClick = (tabButtonId) => {
+    setSelectedTopic(tabButtonId);
+  };
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre><code>{EXAMPLES[selectedTopic].code}</code></pre>
+      </div>
+    );
+  }
+
+  return (
+    <section id="examples">
+      <h2>Examples</h2>
+      <menu>
+        <TabButton isActive={selectedTopic === 'components'} onClick={() => handleTabButtonClick('components')}>Components</TabButton>
+        <TabButton isActive={selectedTopic === 'jsx'} onClick={() => handleTabButtonClick('jsx')}>JSX</TabButton>
+        <TabButton isActive={selectedTopic === 'props'} onClick={() => handleTabButtonClick('props')}>Props</TabButton>
+        <TabButton isActive={selectedTopic === 'state'} onClick={() => handleTabButtonClick('state')}>State</TabButton>
+      </menu>
+      {tabContent}
+    </section>
+  );
+}
